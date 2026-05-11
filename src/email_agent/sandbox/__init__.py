dbc06d5c0f5ada4cdd4d576dbb8394dd02103d0b
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2026-05-11 07:02:30 +0000

    feat(sandbox): add generic sandbox environment

--- src/email_agent/sandbox/__init__.py
+++ src/email_agent/sandbox/__init__.py
@@ -1,4 +1,13 @@
+from email_agent.sandbox.environment import FileStat, SandboxEnvironment, ShellResult
 from email_agent.sandbox.inmemory import InMemorySandbox
+from email_agent.sandbox.inmemory_environment import InMemoryEnvironment
 from email_agent.sandbox.port import AssistantSandbox
 
-__all__ = ["AssistantSandbox", "InMemorySandbox"]
+__all__ = [
+    "AssistantSandbox",
+    "FileStat",
+    "InMemoryEnvironment",
+    "InMemorySandbox",
+    "SandboxEnvironment",
+    "ShellResult",
+]
